4b5067aef8af014270ffb298677785064e2220f4
jafar-aml <jafaragha77@gmail.com>
2019-07-31 20:07:59 +0430

    DataBinding! added to project!

--- app/src/main/java/aml/cefer/diceroller/MainActivity.kt
+++ app/src/main/java/aml/cefer/diceroller/MainActivity.kt
@@ -1,25 +1,23 @@
 package aml.cefer.diceroller
 
+import aml.cefer.diceroller.databinding.ActivityMainBinding
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_btn)
-        diceImage = findViewById(R.id.dice_iv)
 
-        rollButton.text = "Let's Roll"
+        binding.rollBtn.text = "Let's Roll"
 
-        rollButton.setOnClickListener {
+        binding.rollBtn.setOnClickListener {
             rollDice()
         }
 
@@ -27,9 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-        val random = Random.nextInt(6) + 1
-
-        val drawableResource = when (random) {
+        val drawableResource = when (Random.nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -40,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        diceImage.setImageResource(drawableResource)
+        binding.diceIv.setImageResource(drawableResource)
 
 
     }
